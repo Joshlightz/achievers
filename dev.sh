426e02b5f536e00bd8d6cb57536e832efeb84e78
Iphyworld <ifeomaokoli838@gmail.com>
2023-03-04 08:40:15 -0600

    updated dev

--- dev.sh
+++ dev.sh
@@ -5,4 +5,8 @@ I am a tech geek
 # All application will be soft coded.
 # It will be dynamically created
 # Author: Light
+<<<<<<< HEAD
 >>>>>>> development
+=======
+echo " I am a tech geek"
+>>>>>>> b58c3db (resolved)
