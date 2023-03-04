02bba9a83bf84f32dea4f4f46dcd1f7b35de10da
Iphyworld <ifeomaokoli838@gmail.com>
2023-03-04 07:56:46 -0600

    modified files

--- db.sql
+++ db.sql
@@ -1,5 +1,8 @@
- database connection
+ database connection/app
 # This file will back back-up the company's info
 # please do not hard code
+# files are to be created dynamically
 # Author: Light
-
+db-passwd=$passwd
+db-username=$username
+echo " I am a tech geek"
--- dev.sh
+++ dev.sh
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
+I am a tech geek
+=======
 # This file develops application for users
 # All application will be soft coded.
 # It will be dynamically created
 # Author: Light
+<<<<<<< HEAD
 echo " I am a tech geek"
+=======
+>>>>>>> development
+>>>>>>> master
